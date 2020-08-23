ac48685f4c97c722de068c60b48441584d2359ce
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-08-23 02:49:15 -0300

    hot fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "customers#index"
 
   resources :customers, except: [:index] do
-    resources :contacts
+    resources :contacts, only: [:index, :new, :create]
   end
 end
