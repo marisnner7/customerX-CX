d2ff6f9b82a4156b2ba934f81058603cb7dacb49
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-08-22 22:43:36 -0300

    creating customers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts "Cleaning database"
+Customer.destroy_all
+puts "Customers destroyed"
+
+puts "Creating Customers"
+  
+
+30.times do
+  customer = Customer.new(
+    name: Faker::Name.name,
+    telephone: Faker::PhoneNumber.cell_phone_in_e164,
+    register_day: Faker::Date.in_date_period(month: 2)
+  ) 
+  customer.save!
+end
+
+puts 'Finished!'
