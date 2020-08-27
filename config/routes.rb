75c3c3182acd9e46f456a6bae99f5310af78d807
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-08-27 04:49:56 -0300

    add route for omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :customers
+  devise_for :customers,
+  controllers: { omniauth_callbacks: 'customers/omniauth_callbacks' }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
 
