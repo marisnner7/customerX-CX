e64bb80a89177f147ca61999254786f62b0ba992
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-08-27 20:53:32 -0300

    new crud

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,52 +1,53 @@
 class ContactsController < ApplicationController
   before_action :set_contact, only: %i[destroy]
-
-  def index
-    @contacts = Contact.all
-  end  
   
+  def index
+  @contacts = Contact.all
+  end 
+
   def new
-    # we need @customer in our `simple_form_for`
+  # we need @customer in our `simple_form_for`
     @customer = Customer.find(params[:customer_id])
     @contact = Contact.new
   end
-
+  
   def edit
     @customer = Customer.find(params[:customer_id])
-    @contact = @customer.contacts
+    @contact = Contact.find(params[:id])
   end
 
   def update
-    @customer = Customer.find(params[:customer_id])
-    @contact = @customer.contact.find(params[:id])
+    @contact = Contact.find(params[:id])
     @contact.update(contact_params)
-    redirect_to contact_path, notice: 'contact was successfully updated.'
-  end
-
+    redirect_to customer_path(@customer)
+  end  
+  
+  
   def create
     @contact = Contact.new(contact_params)
     # we need `customer_id` to associate contact with corresponding customer
     @customer = Customer.find(params[:customer_id])
     @contact.customer = @customer
     if @contact.save
-      redirect_to customer_path(@customer)
+    redirect_to customer_path(@customer)
     else
-      render :new
+    render :new
     end
   end
-
+  
   def destroy
-    @contact.destroy
-    redirect_to customer_path(@contact.customer)
+  @contact.destroy
+  redirect_to customer_path(@contact.customer)
   end
-
+  
   private
-
+  
   def set_contact
-    @contact = Contact.find(params[:id])
+  @contact = Contact.find(params[:id])
+  end
+  
+  def contact_params
+  params.require(:contact).permit(:name, :emails, :cellphone)
   end
 
-  def contact_params
-    params.require(:contact).permit(:name, :emails, :cellphone)
-  end
 end
