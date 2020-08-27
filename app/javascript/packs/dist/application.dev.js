839644b8c5214a6d5ec693c43f805ec91e402ec7
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-08-27 13:22:57 -0300

    added typed

--- app/javascript/packs/dist/application.dev.js
+++ app/javascript/packs/dist/application.dev.js
@@ -1,24 +1,26 @@
+"use strict";
+
+require("bootstrap");
+
+var _banner = require("../components/banner");
+
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+require("@rails/ujs").start();
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+require("turbolinks").start();
 
-import 'bootstrap';
+require("@rails/activestorage").start();
 
-import { loadDynamicBannerText } from '../components/banner';
+require("channels");
 
-document.addEventListener('turbolinks:load', () => {
+document.addEventListener('turbolinks:load', function () {
   // Call your JS functions here
   // [...]
-  loadDynamicBannerText();
-});
-
-// Uncomment to copy all static images under ../images to the output folder and reference
+  (0, _banner.loadDynamicBannerText)();
+}); // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
