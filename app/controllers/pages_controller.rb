539749b4aa7065db7bf7f5877633c338dde0a965
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-08-27 04:52:10 -0300

    search form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_customer!, only: :home
 
   def home
     @customers = Customer.all
 
-    @customer = if params[:query].present?
-                  Customer.global_search(params[:query])
-                else
-                  Customer.all
-                end
-  end
+    if params[:query].present?
+      @customers = Customer.multisearchable(params[:query])
+    else
+      @customers = Customer.all
+    end
+
+  end  
 end
